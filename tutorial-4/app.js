fedbe7cc2713177f22b4e332baf57214aa015bbe
dhanyn10 <dhanyn.dhan@gmail.com>
2019-01-12 20:12:31 +0700

    doc

--- tutorial-4/app.js
+++ tutorial-4/app.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
+// handling Cross Origin Request
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
