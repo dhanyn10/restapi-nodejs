8439aa0a967ce4f5fff032ced2837833d6d981c0
dhanyn10 <dhanyn.dhan@gmail.com>
2019-01-12 20:15:31 +0700

    doc

--- tutorial-4/app.js
+++ tutorial-4/app.js
@@ -24,6 +24,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
         'Access-Control-Allow-Headers',
+        // below is which kind of headers you want to accept
         'Origin, X-Requested-With, Content-type, Accept, Authorization'
     );
     if(req.method === 'OPTIONS')
